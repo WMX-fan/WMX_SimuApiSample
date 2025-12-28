6fc1b0ef74b06390ab679f85d5f10adc384d9d59
FourTechnos <FourTechnos@DESKTOP-N73IO3S>
2025-12-28 18:41:11 +0900

    プロジェクト ファイルを追加します。

---
+++ WMX_SimuApiSample/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace WMX_SimuApiSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputONbutton = new System.Windows.Forms.Button();
+            this.InputGroup = new System.Windows.Forms.GroupBox();
+            this.InputOFFbutton = new System.Windows.Forms.Button();
+            this.InputAddres = new System.Windows.Forms.NumericUpDown();
+            this.InputGroup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.InputAddres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputONbutton
+            // 
+            this.InputONbutton.Location = new System.Drawing.Point(39, 67);
+            this.InputONbutton.Name = "InputONbutton";
+            this.InputONbutton.Size = new System.Drawing.Size(63, 63);
+            this.InputONbutton.TabIndex = 0;
+            this.InputONbutton.Text = "ON";
+            this.InputONbutton.UseVisualStyleBackColor = true;
+            this.InputONbutton.Click += new System.EventHandler(this.InputONbutton_Click);
+            // 
+            // InputGroup
+            // 
+            this.InputGroup.Controls.Add(this.InputOFFbutton);
+            this.InputGroup.Controls.Add(this.InputAddres);
+            this.InputGroup.Controls.Add(this.InputONbutton);
+            this.InputGroup.Location = new System.Drawing.Point(22, 27);
+            this.InputGroup.Name = "InputGroup";
+            this.InputGroup.Size = new System.Drawing.Size(239, 148);
+            this.InputGroup.TabIndex = 1;
+            this.InputGroup.TabStop = false;
+            this.InputGroup.Text = "デジタル入力";
+            // 
+            // InputOFFbutton
+            // 
+            this.InputOFFbutton.Location = new System.Drawing.Point(137, 67);
+            this.InputOFFbutton.Name = "InputOFFbutton";
+            this.InputOFFbutton.Size = new System.Drawing.Size(63, 63);
+            this.InputOFFbutton.TabIndex = 3;
+            this.InputOFFbutton.Text = "OFF";
+            this.InputOFFbutton.UseVisualStyleBackColor = true;
+            this.InputOFFbutton.Click += new System.EventHandler(this.InputOFFbutton_Click);
+            // 
+            // InputAddres
+            // 
+            this.InputAddres.DecimalPlaces = 1;
+            this.InputAddres.Location = new System.Drawing.Point(39, 25);
+            this.InputAddres.Name = "InputAddres";
+            this.InputAddres.Size = new System.Drawing.Size(116, 19);
+            this.InputAddres.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 186);
+            this.Controls.Add(this.InputGroup);
+            this.Name = "Form1";
+            this.Text = "WMX_SimuApiSample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.InputGroup.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.InputAddres)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InputONbutton;
+        private System.Windows.Forms.GroupBox InputGroup;
+        private System.Windows.Forms.Button InputOFFbutton;
+        private System.Windows.Forms.NumericUpDown InputAddres;
+    }
+}
+
---
+++ WMX_SimuApiSample/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+using WMX3ApiCLR;
+using WMX3ApiCLR.SimuApiCLR;
+
+namespace WMX_SimuApiSample
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>モーションコントロール</summary>
+        private WMX3Api Device;
+
+        /// <summary>モーションコントロールシミュレーター</summary>
+        WMX3ApiCLR.SimuApiCLR.Simu SimulatorDevice;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Device = new WMX3Api();
+            SimulatorDevice = new Simu(Device);
+            Disposed += Form1_Disposed;
+        }
+
+        private void Form1_Disposed(object sender, EventArgs e)
+        {
+            Device?.CloseDevice();
+            Device?.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DevicesInfo devInfo = new DevicesInfo();
+            Device.CreateDevice("C:\\Program Files\\SoftServo\\WMX3\\",
+                DeviceType.DeviceTypeNormal,
+                0xFFFFFFFF);
+
+            Device.SetDeviceName("WMX_SimuApiSample");
+            Device.GetAllDevices(ref devInfo);
+        }
+
+
+        /// <summary>入力アドレス(@byte,bit)</summary>
+        //const float IN_SW0 = 1.2f;  
+
+        private void InputONbutton_Click(object sender, EventArgs e)
+        {
+            SetInputValue((float)InputAddres.Value, true);
+        }
+
+        private void InputOFFbutton_Click(object sender, EventArgs e)
+        {
+            SetInputValue((float)InputAddres.Value, false);
+        }
+
+        /// <summary>
+        /// 入力値の書き換え
+        /// </summary>
+        /// <param name="addres">操作する対象</param>
+        /// <param name="value">値</param>
+        public void SetInputValue(float addres ,bool value)
+        {
+            string sbit = addres.ToString();
+            int dotIndex = sbit.IndexOf('.');
+            int bit = 0;
+            if (dotIndex != -1)
+            {
+                string fractionPart = sbit.Substring(dotIndex + 1);
+                bit = int.Parse(fractionPart);
+            }
+
+            SimulatorDevice.SetInBit((int)addres, bit, value ? (byte)1 : (byte)0);
+        }
+    }
+}
